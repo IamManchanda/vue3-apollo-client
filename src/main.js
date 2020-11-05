0aad6ceadbbb7614160333113447af470fd50dcf
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-11-05 17:23:18 +0530

    Starting work on the boilerplate

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+/* import { ApolloClient, InMemoryCache } from "@apollo/client"; */
 import "./registerServiceWorker";
 import router from "./router";
+
+/* const defaultClient = new ApolloClient({
+  uri: "https://48p1r2roz4.sse.codesandbox.io",
+  cache: new InMemoryCache(),
+}); */
 
 createApp(App)
   .use(router)
