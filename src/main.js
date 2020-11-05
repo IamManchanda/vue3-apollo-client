5409ef8af62f0812a6c4cd184631adb3f852dc9d
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-11-05 22:57:58 +0530

    More work on graphql

--- src/main.js
+++ src/main.js
@@ -1,13 +1,29 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-/* import { ApolloClient, InMemoryCache } from "@apollo/client"; */
+import { ApolloClient, gql, InMemoryCache } from "@apollo/client";
 import "./registerServiceWorker";
 import router from "./router";
 
-/* const defaultClient = new ApolloClient({
-  uri: "https://48p1r2roz4.sse.codesandbox.io",
+const defaultClient = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
   cache: new InMemoryCache(),
-}); */
+});
+
+const query = gql`
+  query AllWines {
+    allWines {
+      title
+      description
+      variety
+    }
+  }
+`;
+
+defaultClient
+  .query({
+    query,
+  })
+  .then(res => console.log(res));
 
 createApp(App)
   .use(router)
